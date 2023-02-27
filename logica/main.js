653445066cb5d21d2dae9093d90d0749e091b15f
cinabrio <66962454+cinabrio@users.noreply.github.com>
2023-02-26 23:32:03 -0300

    Add files via upload

---
+++ logica/main.js
@@ -0,0 +1,40 @@
+console.log(screen.width)
+
+
+
+let = pantalla = screen.width
+
+
+
+const enlaces = Array.from(document.getElementsByTagName("i"))
+const botonesReferencia = Array.from(document.getElementsByClassName("ref")) 
+
+
+
+if(pantalla <= 500){
+    enlaces.forEach(e =>{
+        e.addEventListener("click", (e)=>{
+            const padre = e.target.parentNode;
+            const hijo = padre.children[1]
+            
+            for (let index = 0; index < enlaces.length; index++) {
+                enlaces[index].classList.remove("contacto")
+                const element = enlaces[index].parentNode.children[1];
+                element.classList.replace("contacto-visible","contacto")
+                console.log(element)
+                
+            }
+            hijo.classList.toggle("contacto-visible")
+            e.target.classList.toggle("contacto")
+        })
+    })
+}
+
+
+botonesReferencia.forEach(e=>{
+    e.addEventListener("click", (e)=>{
+        const padre = e.target.parentNode;
+        padre.children[2].classList.toggle("ref-visible")
+        e.target.textContent = "cerrar" 
+    })
+})
